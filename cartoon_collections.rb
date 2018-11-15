afda93c17640e57ff48d6fac668a91b42e8c3a14
Saad Malik <saad.t.malik@gmail.com>
2018-11-15 20:13:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |name, index|
-    index =+ 1
-    puts "#{index+1}, #{name}"
+    index += 1
+    puts "#{index}. #{name}"
   end
 end
 
